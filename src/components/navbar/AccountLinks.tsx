7bbe8ef02ca54f95317b72d6f5040515ac63fcb9
abdullah <abdullah981066@gmail.com>
2025-06-05 07:07:31 +0530

    Remove unnecessary 'asChild' prop from Logout button in AccountLinks component

--- src/components/navbar/AccountLinks.tsx
+++ src/components/navbar/AccountLinks.tsx
@@ -45,7 +45,6 @@ const AccountLinks = () => {
               window.location.reload();
             })
           }
-          asChild
         >
           <LogOut className="h-4 w-4 mr-2" /> Logout
         </Button>
